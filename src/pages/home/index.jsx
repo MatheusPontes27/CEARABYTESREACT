48540d9384bfb6de89cbf9ef2a66830d85c4f423
MatheusPontes27 <matheusemmanuel71@gmail.com>
2025-05-26 10:29:31 -0300

    atualizando a home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Title>Bem-vindo à CearáBytes</Title>
+      <p>Estamos construindo o futuro da tecnologia.</p>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 2rem;
+  text-align: center;
+  background-color: #f2f2f2;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #0077cc;
+`;
